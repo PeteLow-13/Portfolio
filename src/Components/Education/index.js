42080644cb8c56fe624ac194219e5df1b6b830cc
Pete Low <pedritolow@gmail.com>
2021-02-03 11:02:35 -0800

    Made seals display

--- src/Components/Education/index.js
+++ src/Components/Education/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './style.css';
+import UW from './uwseal.png' ;
+import UH from './uhsealgold.jpg' ;
 
 function Education() {
     return (
         <div className='education-container'>
-            <h2>University of Washington</h2>
+            <h2>Education</h2>
+            <div className='seal-div'>
+            <img className='school-seal' src={UW} alt='UW seal' />
+            </div> 
+            <h3>University of Washington</h3>
             <p>Certificate: Full-stack Web Development</p>
-            <h2>University of Hawaii Manoa</h2>
+            <div className='seal-div'>
+            <img className='school-seal' src={UH} alt='UH seal' />
+            </div> 
+            <h3>University of Hawaii Manoa</h3>
             <p>Bachelors Political Science, with emphasis on Spanish</p>
         </div>
     )
