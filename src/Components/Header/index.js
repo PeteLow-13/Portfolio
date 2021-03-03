8f7e9a8a1d068857b75e0aa2d5fb702d704a078a
Pete Low <pedritolow@gmail.com>
2021-03-02 16:11:47 -0800

    added server

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -9,12 +9,13 @@ function Header() {
             <div className='profile-picture-div'>
                 <img className='profile-picture' src={img} alt='Pete Low' />
             </div>
-            <p>Certified Full-stack Developer.</p>
+            <p>Certified Full-Stack Developer.</p>
             <div className='social'>
                 <a href='https://www.linkedin.com/in/pete-low/' target='blank'><img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" alt='LinkedIn'/></a>
                 <a href='https://github.com/PeteLow-13' target='blank'><img src="https://img.icons8.com/ios-filled/50/000000/github.png" alt='Github'/></a>
             </div>
-            <a href='https://docs.google.com/document/d/1FtLA_F3kEbFwN_glaEtvBXf4-1VkjcCjs5xS0pcoej8/edit?usp=sharing' target='blank'>Check out my Resume here</a>
+            <p>Check out my <a href='https://docs.google.com/document/d/1FtLA_F3kEbFwN_glaEtvBXf4-1VkjcCjs5xS0pcoej8/edit?usp=sharing' target='blank'>Resume</a></p>
+            
         </div>
     )
 }
