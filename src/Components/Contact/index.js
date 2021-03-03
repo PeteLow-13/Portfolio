ce27e3ed78f8fa00b54046cbc7b6574b89785e82
Pete Low <pedritolow@gmail.com>
2021-03-02 20:33:32 -0800

    added project images

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -5,14 +5,10 @@ function Contact() {
     return (
         <div className='contact-container'>
             <h2>Let's get in touch</h2>
-            <ul>
                 <p>Phone: <a href='tel:+12066178254'>(206)-617-8254</a></p>
                 <p>Email: <a href='mailto:pedritolow@gmail.com'>pedritolow@gmail.com</a></p>
                 <p>LinkedIn: <a href='https://www.linkedin.com/in/pete-low/'>Pete Low</a> </p>
-                <p>Github: <a href='https://github.com/PeteLow-13'>PeteLow-13</a></p>
-            </ul>
-
-            
+                <p>Github: <a href='https://github.com/PeteLow-13'>PeteLow-13</a></p>   
         </div>
     )
 }
