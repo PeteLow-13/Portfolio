302233b7632f98e9c782f5a35206bfcc32cf2c7d
Pete Low <pedritolow@gmail.com>
2021-04-14 10:36:34 -0700

    portfolio additions

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Portfolio from './Components/Portfolio';
 import Skills from './Components/Skills';
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+// import 'react-pro-sidebar/dist/css/styles.css';
 
 function App() {
   return (
     <div className="App">
+      
+
+      {/* <ProSidebar>
+        <Menu iconShape="square">
+          <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
+          <SubMenu title="Components" icon={<FaHeart />}>
+            <MenuItem>Component 1</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+          </SubMenu>
+        </Menu>
+      </ProSidebar>; */}
       <Header />
       <Skills />
       <Portfolio />
--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import './style.css';
+import img1 from './images/sk8tool.png';
+import img2 from './images/funapp.png';
+import img3 from './images/weatherdashboard.png';
+import img4 from './images/burgerapp.png';
+// import title from 'portfolio.json';
 
 function Portfolio() {
     return (
         <div className='portfolio-container'>
             <h2>Portfolio</h2>
-            
+              <div className='project-container'>
+                  <h3 className='title'>Sk8Tool</h3>
+                  <img className='portfolio-img' src={img1} alt='sk8tool picture'/>
+                  <div className='description'><p>Sk8Tool is a project that allows Skateboarders to Find Skateparks, Skateshops and also view and add Skate Spots to our database</p></div>
+                  <div className='technologies'>JavaScript, CSS3, HTML5, HandlebarsJS, Google Maps API, Open Weather API</div>
+                  <div><a href='https://github.com/PeteLow-13/Project-02-Sk8Tool' target='blank'>Git hub Repository</a></div>
+                  <div><a href='https://sk8fndr.herokuapp.com/' target='blank'>Deployed</a></div>
+              </div>
+              <div className='project-container'>
+                  <h3 className='title'>Fun App</h3>
+                  <img className='portfolio-img' src={img2} alt='Funapp Picture'/>
+                  <div className='description'><p>An app for parents to use to provide safe prescreened content for their children.</p></div>
+                  <div className='technologies'>JavaScript, HTML5, SCSS, CSS3</div>
+                  <div><a href='https://github.com/PeteLow-13/FunApp' target='blank'>Git hub Repository</a></div>
+                  <div><a href='https://project03familyapp.herokuapp.com/' target='blank'>Deployed</a></div>
+              </div>
+              <div className='project-container'>
+                  <h3 className='title'>Weather App</h3>
+                  <img className='portfolio-img' src={img3} alt='Weather App Picture'/>
+                  <div className='description'><p>A tool to check the weather and the the weather forcast for any city</p></div>
+                  <div className='technologies'>JavaScript, HTML5, CSS3, Open Weather API</div>
+                  <div><a href='https://github.com/PeteLow-13/WeatherApp' target='blank'>Git hub Repository</a></div>
+                  <div><a href='https://petelow-13.github.io/WeatherApp/' target='blank'>Deployed</a></div>
+              </div>
+              <div className='project-container'>
+                  <h3 className='title'>Burger Party!</h3>
+                  <img className='portfolio-img' src={img4} alt='Burger App Picture'/>
+                  <div className='description'><p>A fun little app to create burgers you would like to eat and save them until you devour them!</p></div>
+                  <div className='technologies'>JavaScript, HTML5, CSS3, Open Weather API</div>
+                  <div><a href='https://github.com/PeteLow-13/BurgerParty' target='blank'>Git hub Repository</a></div>
+                  <div><a href='https://morning-meadow-19398.herokuapp.com/' target='blank'>Deployed</a></div>
+              </div>
         </div>
     )
     }
